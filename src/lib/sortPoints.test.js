2d25d33c572bc8c732ad7243ff68c2ce23de38f6
Kwispy <bcaples12@gmail.com>
2024-03-04 19:29:20 -0800

    init + wrote first test for sorting points

---
+++ src/lib/sortPoints.test.js
@@ -0,0 +1,16 @@
+const sortPoints = require('./sortPoints')
+
+test("Testing input of [{x: 1, y: 3}, {x: 4, y: 9}, {x: 3, y: 10}] to sort to [{x: 1, y: 3},{x: 3, y: 10},{x: 4, y: 9}]", function(){
+    points = [
+        {x: 1, y: 3}, 
+        {x: 4, y: 9}, 
+        {x: 3, y: 10},
+    ],
+    
+    expect(sortPoints(points)).toStrictEqual([
+        {x: 1, y: 3},
+        {x: 3, y: 10}, 
+        {x: 4, y: 9}, 
+        
+    ])
+})
