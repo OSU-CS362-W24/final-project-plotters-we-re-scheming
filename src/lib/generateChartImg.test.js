c7b932d0902086142116bd55ac66611c7348d02e
Teo Ippolito <ippolitt@oregonstate.edu>
2024-03-05 15:00:50 -0800

    Configured jest/jsDom test enviornment

--- src/lib/generateChartImg.test.js
+++ src/lib/generateChartImg.test.js
@@ -1,1 +1,27 @@
-// Unit Tests for GenerateChartImg.js
+// Unit Tests for GenerateChartImg.js using jest/jsdom
+
+/**
+* @jest-environment jsdom
+*/
+require("whatwg-fetch")
+
+const fs = require("fs")
+
+const domTesting = require("@testing-library/dom")
+require("@testing-library/jest-dom")
+
+const userEvent = 
+require("@testing-library/user-event").default
+
+
+
+function initDomFromFiles(htmlPath, jsPath){
+    const html = fs.readFileSync(htmlPath, 'utf8')
+    document.open()
+    document.write(html)
+    document.close()
+    jest.isolateModules(function(){
+        require(jsPath)
+    })
+}
+
