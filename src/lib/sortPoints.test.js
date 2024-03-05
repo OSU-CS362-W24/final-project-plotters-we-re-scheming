3bd717ceee328a5c58fc7dec8f225fa5d593c0c4
Kwispy <bcaples12@gmail.com>
2024-03-04 19:44:31 -0800

    Wrote another test (with negative values)

--- src/lib/sortPoints.test.js
+++ src/lib/sortPoints.test.js
@@ -5,12 +5,22 @@ test("Testing input of [{x: 1, y: 3}, {x: 4, y: 9}, {x: 3, y: 10}] to sort to [{
         {x: 1, y: 3}, 
         {x: 4, y: 9}, 
         {x: 3, y: 10},
-    ],
+    ]
     
     expect(sortPoints(points)).toStrictEqual([
         {x: 1, y: 3},
         {x: 3, y: 10}, 
         {x: 4, y: 9}, 
         
+    ])
+})
+
+test("Testing input of [{x: 1, y: 3}, {x: -4, y: 9}, {x: -20, y: 10}] to sort to [{x: -20, y: 10}, {x: -4, y: 9}, {x: 1, y: 3}]", function(){
+    points = [{x: 1, y: 3}, {x: -4, y: 9}, {x: -20, y: 10}]
+    
+    expect(sortPoints(points)).toStrictEqual([
+        {x: -20, y: 10},
+        {x: -4, y: 9},
+        {x: 1, y: 3}
     ])
 })
