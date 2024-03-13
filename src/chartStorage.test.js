9fde076599220f55e1dacddc1cc2d6a10773f7e1
teagansim <teagansimoneau@gmail.com>
2024-03-13 13:30:33 -0700

    Init: environment set up

---
+++ src/chartStorage.test.js
@@ -0,0 +1,35 @@
+/**
+ * @jest-environment jsdom
+ */
+
+
+// import functions
+const { saveChart, loadAllSavedCharts, loadSavedChart, updateCurrentChartData, loadCurrentChartData }= require('./lib/chartStorage')
+
+// create three distinct charts to test
+const chartOne = {
+    type: "plot",
+    data: "dataOne",
+    xLabel: "xLabelOne",
+    yLabel: "yLabelOne",
+    title: "graphOne",
+    color: "red"
+}
+
+const chartTwo = {
+    type: "scatter",
+    data: "dataTwo",
+    xLabel: "xLabelTwo",
+    yLabel: "yLabelTwo",
+    title: "graphTwo",
+    color: "blue"
+}
+
+const chartThree = {
+    type: "bar",
+    data: "dataThree",
+    xLabel: "xLabelThree",
+    yLabel: "yLabelThree",
+    title: "plotThree",
+    color: "green"
+}
